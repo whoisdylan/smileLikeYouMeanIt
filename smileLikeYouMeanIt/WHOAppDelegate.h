7bbe5814fc5264d0cfe086bab4bfe024f1b1d11c
Dylan Koenig <koenigup@gmail.com>
2014-03-29 19:41:01 -0400

    Initial Commit

---
+++ smileLikeYouMeanIt/WHOAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  WHOAppDelegate.h
+//  smileLikeYouMeanIt
+//
+//  Created by dylan on 3/29/14.
+//  Copyright (c) 2014 whoisdylan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WHOAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ smileLikeYouMeanIt/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  smileLikeYouMeanIt
+//
+//  Created by dylan on 3/29/14.
+//  Copyright (c) 2014 whoisdylan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "WHOAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([WHOAppDelegate class]));
+    }
+}
